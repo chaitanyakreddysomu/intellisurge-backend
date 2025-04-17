db8b56d5e3a9eda5c541d957fdcf2d1d2b565180
SomuChaitanyaKumarReddy <145319559+SomuChaitanyaKumarReddy@users.noreply.github.com>
2025-04-17 17:25:37 +0530

    s

--- core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Blog, JobNotification,AdminUser,Contact,OurTeam,OurTeamCareer
-
-admin.site.register(Blog)
-admin.site.register(JobNotification)
-admin.site.register(AdminUser)
-admin.site.register(Contact)
-admin.site.register(OurTeam)
-admin.site.register(OurTeamCareer)
